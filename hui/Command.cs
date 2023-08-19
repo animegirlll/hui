afc749cbedb7281fe470aa1ef479d3e767f552ab
Никита Любшин <dj-nekit002@mail.ru>
2023-08-19 19:05:44 +0300

    бэбра

--- hui/Command.cs
+++ hui/Command.cs
@@ -7,21 +7,26 @@ namespace hui
     [CommandHandler(typeof(ClientCommandHandler))]
     internal class damage : ICommand
     {
-        public string Command => "changenickname";
-        public string[] Aliases => new string[] {"cn", "changenick"};
-        public string Description => "Меняет ваш уебанский никнейм";
+        public string Command => "changeinfo";
+        public string[] Aliases => new string[] {"ci"};
+        public string Description => "Меняет информацию об игровом персонаже";
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
             var player = Player.Get(sender);
-            if(arguments.Count != 1)
+            if(arguments.Count < 1)
             {
                 response = "Еблан?";
                 return false;
             }
             else
             {
-                player.CustomName = arguments[0];
-                response = "Никнейм изменён";
+                string name = arguments[0];
+                for(int i = 1; i < arguments.Count; i++)
+                {
+                    name += " " + arguments[i];
+                }
+                player.CustomName = name;
+                response = "Информация изменена";
                 return true;
             }
         }
---
+++ hui/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Server;
+using PluginAPI;
+using Exiled.API.Interfaces;
+
+namespace hui
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            RegisterEvents();
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+        public void RegisterEvents()
+        {
+        }
+        void Respawn()
+        {
+
+        }
+    }
+}
